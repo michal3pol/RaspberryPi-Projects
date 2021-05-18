c73e692048b168d0a81c2f47be9a3a0764cce587
michal3pol <michalfoks3@gmail.com>
2021-05-19 00:00:36 +0200

    measuringtemp_hum_dht11

---
+++ Humidity_Temperature/htmain.py
@@ -0,0 +1,21 @@
+import dht11 #source: https://pypi.org/project/dht11/
+import RPi.GPIO as GPIO
+import time
+
+# initialize GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+instance = dht11.DHT11(pin = 4)
+
+while True:
+    result = instance.read()
+    if result.is_valid():
+        print("Temperature: %-3.1f C" % result.temperature)
+        print("Humidity: %-3.1f %%" % result.humidity)
+    else:
+        print("Error: %d" % result.error_code)
+    time.sleep(3)
+    
+GPIO.cleanup()
